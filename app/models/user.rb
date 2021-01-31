30aa0153741ff8411fc93ec4485be9a7443368f5
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-01-31 15:35:39 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  enum role: { super_admin: 1, accountant: 2, driver: 3 }
+  enum role: { super_admin: 1, accountant: 2, warehouse: 3, manager: 4, driver: 5 }
+  # super_admin : Админ
+  # accountant : Нягтлан
+  # warehouse : агуулахын ажилтан
+  # manager : Захиалга оруулах ажилтан
+  # driver: Хүргэлтийн жолооч
   devise :database_authenticatable,
          :recoverable, :rememberable
   validates :first_name, presence: true

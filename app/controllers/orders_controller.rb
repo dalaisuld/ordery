c5771de2063a70ad1c3b7d7ce75abd948b58d744
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-02-04 13:42:45 +0800

    code format

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,19 +2,18 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @page_title = "Захиалгийн жагсаалт"
+    @page_title = 'Захиалгийн жагсаалт'
     @page_orders_active = true
   end
 
   def list
-    order_by = "id desc"
+    order_by = 'id desc'
     if params[:sortField].present? && params[:sortOrder].present?
       order_by = "#{params[:sortField]} #{params[:sortOrder]} "
     end
-
-    orders = Order.all.page(params[:pageIndex]).per(params[:pageSize]).order(order_by)
-
     order_count = Order.search_by(params).count
-    render json: { data: orders, itemsCount: order_count}
+    orders = Order.joins('LEFT JOIN products ON products.id = orders.product_id').select('orders.*, products.name, products.price').page(params[:pageIndex]).per(params[:pageSize]).order(order_by)
+    orders = orders.search_by(params)
+    render json: { data: orders, itemsCount: order_count }
   end
 end

340802ea79d440e01681f0d92172480b34e4ebeb
bayan suld <bayankhishigt_dalaisuld@unimedia.co.jp>
2021-02-08 13:23:50 +0800

    error fixed

--- app/models/items_import.rb
+++ app/models/items_import.rb
@@ -22,21 +22,25 @@ class ItemsImport
     end
   
     def save(user_id)
-      spreadsheet = open_spreadsheet
-      header = spreadsheet.row(2)
-      (3..spreadsheet.last_row).map do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-        if spreadsheet.row(i)[1] != 0
-          ActiveRecord::Base.transaction do
-            Order.create({transition_date: spreadsheet.row(i)[0],
-                          user_id: user_id,
-                          total_amount: spreadsheet.row(i)[1] * 1,
-                          amount: spreadsheet.row(i)[1], 
-                          description: spreadsheet.row(i)[2], 
-                          account_number: spreadsheet.row(i)[3]})
+      begin
+        spreadsheet = open_spreadsheet
+        header = spreadsheet.row(2)
+        (3..spreadsheet.last_row).map do |i|
+          row = Hash[[header, spreadsheet.row(i)].transpose]
+          if spreadsheet.row(i)[1] != 0
+            ActiveRecord::Base.transaction do
+              Order.create({transition_date: spreadsheet.row(i)[0],
+                            user_id: user_id,
+                            total_amount: spreadsheet.row(i)[1] * 1,
+                            amount: spreadsheet.row(i)[1], 
+                            description: spreadsheet.row(i)[2], 
+                            account_number: spreadsheet.row(i)[3]})
+            end
           end
         end
+        true
+      rescue => e
+        false
       end
-      false
     end
   end

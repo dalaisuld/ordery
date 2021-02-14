2f037c66f5935c1ee2cb23b3ff31b24e63f31de2
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-02-15 02:02:31 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,20 +5,8 @@ set :repo_url, "git@github.com:dalaisuld/ordery.git"
 set :deploy_to, "/home/deploy/#{fetch :application}"
 set :branch, 'design-init'
 
-before "deploy:assets:precompile", "deploy:yarn_install"
 
-namespace :deploy do
-  desc 'Run rake yarn:install'
-  task :yarn_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install")
-      end
-    end
-  end
-end
-
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', 'public/packs', 'node_modules'
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
@@ -26,3 +14,9 @@ set :keep_releases, 5
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
+#
+#
+set :default_env, {
+    PATH: '$HOME/.npm-packages/bin/:$PATH',
+    NODE_ENVIRONMENT: 'production'
+}

e1d98f82f656236404938595f8749f4ffb92f2be
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-02-14 18:54:30 +0800

    router

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   resources :orders
+  resources :sms_logs
   resources :items_imports, only: [:new, :create]
   post 'users/update_password', :to => 'users#update_password'
   post 'products/list', :to => 'products#list'
+  post 'sms_logs/list', :to => 'sms_logs#list'
   post 'orders/list', :to => 'orders#list'
   post 'orders/add_cargo', :to => 'orders#add_cargo'
 

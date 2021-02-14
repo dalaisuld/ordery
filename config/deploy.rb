70a0d91a2e4661124969773dab0172b868f2321c
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-02-15 01:58:10 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,19 @@ set :repo_url, "git@github.com:dalaisuld/ordery.git"
 set :deploy_to, "/home/deploy/#{fetch :application}"
 set :branch, 'design-init'
 
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Only keep the last 5 releases to save disk space

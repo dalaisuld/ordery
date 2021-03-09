5d34563d442ed93f9b51950cd252a564ccd4fbee
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-03-09 22:28:28 +0800

    fixxx

--- app/models/items_import.rb
+++ app/models/items_import.rb
@@ -28,7 +28,7 @@ class ItemsImport
       (3..spreadsheet.last_row).map do |i|
         row = Hash[[header, spreadsheet.row(i)].transpose]
         if spreadsheet.row(i)[1] != 0
-          Order.transaction do
+          ActiveRecord::Base.transaction do
             transition_date = spreadsheet.row(i)[0].gsub('.', '/')
             amount = spreadsheet.row(i)[1]
             description = spreadsheet.row(i)[2].to_s
@@ -42,25 +42,25 @@ class ItemsImport
             order.description = description
             order.phone_number = phone_number
             order.account_number = account_number
-            product_id_quantities.each do |product_id_quantity|
-              product_id = product_id_quantity.split('-')[0]
-              quantity = product_id_quantity.split('-')[1]
-              product = Product.find(product_id)
-              if product.present?
-                order_detail = OrderDetail.new
-                order_detail.order = order
-                order_detail.product = product
-                order_detail.quantity = quantity
-                order_detail.price = product.price
-                is_willing_cnt = OrderDetail.where(status: IS_WILLING).count
-                product.quantity - is_willing_cnt  >= quantity.to_i ? (order_detail.status = IS_WILLING) : (order_detail.status = IS_WAITING)
-                order_detail.save!
-              end
-            end
             order.save!
-            #  Хэрэглэгчийн бүртгэл үүсгэх хэсэг
-            Client.create!(phone_number: phone_number) if Client.where(phone_number: phone_number).count == 0 && phone_number.present?
           end
+          product_id_quantities.each do |product_id_quantity|
+            product_id = product_id_quantity.split('-')[0]
+            quantity = product_id_quantity.split('-')[1]
+            product = Product.find(product_id)
+            if product.present?
+              order_detail = OrderDetail.new
+              order_detail.order = order
+              order_detail.product = product
+              order_detail.quantity = quantity
+              order_detail.price = product.price
+              is_willing_cnt = OrderDetail.where(status: IS_WILLING).count
+              product.quantity - is_willing_cnt  >= quantity.to_i ? (order_detail.status = IS_WILLING) : (order_detail.status = IS_WAITING)
+              order_detail.save!
+            end
+          end
+          #  Хэрэглэгчийн бүртгэл үүсгэх хэсэг
+          Client.create!(phone_number: phone_number) if Client.where(phone_number: phone_number).count == 0 && phone_number.present?
         end
       end
       true

d99a85d85275cefa7818773a2da23b8b8875ab29
bayan suld <bayankhishigt_dalaisuld@unimedia.co.jp>
2021-03-17 20:15:41 +0800

    log change

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,6 +3,15 @@ class LogsController < ApplicationController
         @page_title = 'Түүх'
         @page_logs_active = true
         @categoris = Category.order('id desc')
-        @logs = Log.all
+    end
+
+    def create
+        logs = Log.select("created_at, description, user_id, (select first_name from users where id = user_id) as username").
+        search_by(params).page(params[:pageIndex]).per(params[:pageSize])
+        logs_count = Log.all
+        logs.each do |log|
+            log['created_at'].strftime("%Y%M%d")
+        end
+        render json: { data: logs, itemsCount: logs_count}
     end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,13 @@
 class Log < ApplicationRecord
+    def self.search_by(search)
+        by_description(search['description'])
+    end
+
+    def self.by_description(description)
+        if description.present?
+            where('description LIKE ?', "%#{description}%")
+          else
+            all
+          end
+    end
 end

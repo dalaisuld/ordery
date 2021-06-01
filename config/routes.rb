299be4417bb8d380a713fc69c0ee95b98e327994
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-06-01 17:57:13 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :deliveries
   resources :sms_logs
+  resources :reports
   resources :histories
   resources :items_imports, only: [:new, :create]
   resources :logs

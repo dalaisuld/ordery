c35e90aa8c3ecef170e9725ba3679b8c64f89f43
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-08-19 15:17:46 +0800

    fix

--- app/controllers/sms_logs_controller.rb
+++ app/controllers/sms_logs_controller.rb
@@ -8,7 +8,8 @@ class SmsLogsController < ApplicationController
 
   def list
     order_by = 'created_at desc'
+    sms_logs_count = SmsLog.count
     sms_logs = SmsLog.all.page(params[:pageIndex]).per(params[:pageSize]).order(order_by)
-    render json: { data: sms_logs, itemsCount: sms_logs.count }
+    render json: { data: sms_logs, itemsCount: sms_logs_count }
   end
 end

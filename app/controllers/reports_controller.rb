daa5f5fa4caa7cc398747072d4d6857eb750b0aa
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-10-09 01:22:31 +0800

    gi

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,6 @@
 class ReportsController < ApplicationController
   def index
-
     @reports = OrderDetail.where(finish_date: Time.now.strftime('%Y-%m-%d')).group('action_user_id').sum(:cargo_price)
-
-    @page_report_active = true
-    @cargo_today_total = OrderDetail.select('SUM(cargo_price) AS total').where('finish_date like :q', q: "%#{Time.now.strftime('%Y-%m-%d')}%").first
-    @cargo_today_total_cash = OrderDetail.select('SUM(cargo_price) AS total').where('finish_date like :q and is_cash = :is_cash', q: "%#{Time.now.strftime('%Y-%m-%d')}%", is_cash: "1" ).first
-    @cargo_today_total_transaction = OrderDetail.select('SUM(cargo_price) AS total').where('finish_date like :q and is_cash = :is_cash', q: "%#{Time.now.strftime('%Y-%m-%d')}%", is_cash: "0" ).first
-
     @cargo_today = OrderDetail.where(finish_date: Time.now.strftime('%Y-%m-%d')).sum(:cargo_price)
   end
 end

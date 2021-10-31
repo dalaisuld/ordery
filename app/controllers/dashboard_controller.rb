b059216fe5d7cec36e110f83ad0840a561083a20
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-10-31 15:34:54 +0800

    fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,26 +12,9 @@ class DashboardController < ApplicationController
     @canceled_order = OrderDetail.where(status: IS_CANCELED).count
     @products = Product.count
     @customers = Client.count
-    @ordered_products = OrderDetail.joins('as od inner join products as p on p.id = od.product_id')
-                                      .select('p.id, p.name, p.price, p.quantity, SUM(od.quantity) as all_product,
-                          SUM(case when od.status = 0 then od.quantity else 0 end) as is_waiting,
-                          SUM(case when od.status = 1 then od.quantity else 0 end) as is_willing,
-                          SUM(case when od.status = 2 then od.quantity else 0 end) as is_delivery,
-                          SUM(case when od.status = 3 then od.quantity else 0 end) as is_finish,
-                          SUM(case when od.status = 4 then od.quantity else 0 end) as is_cancelled')
-                                      .group('p.id, p.name')
 
-    @cargo_today = OrderDetail.select('SUM(cargo_price) AS total,
-    SUM(CASE WHEN is_cash = 0 THEN cargo_price ELSE 0 END) AS cash,
-    SUM(CASE WHEN is_cash = 1 THEN cargo_price ELSE 0 END) AS transfer').where('delivery_date like :q or finish_date like :q', q: "%#{Time.now.strftime('%Y-%m-%d')}%").first
-
-    @cargo_yesterday = OrderDetail.select('SUM(cargo_price) AS total,
-    SUM(CASE WHEN is_cash = 0 THEN cargo_price ELSE 0 END) AS cash,
-    SUM(CASE WHEN is_cash = 1 THEN cargo_price ELSE 0 END) AS transfer').where('delivery_date like :q or finish_date like :q', q: "%#{Date.today.prev_day.strftime('%Y-%m-%d')}%").first
-
-    @cargo_day_before_yesterday = OrderDetail.select('SUM(cargo_price) AS total,
-    SUM(CASE WHEN is_cash = 0 THEN cargo_price ELSE 0 END) AS cash,
-    SUM(CASE WHEN is_cash = 1 THEN cargo_price ELSE 0 END) AS transfer').where('delivery_date like :q or finish_date like :q', q: "%#{Date.today.prev_day.prev_day.strftime('%Y-%m-%d')}%").first
+    @reports = OrderDetail.where(finish_date: Time.now.strftime('%Y-%m-%d')).group('action_user_id').sum(:cargo_price)
+    @cargo_today = OrderDetail.where(finish_date: Time.now.strftime('%Y-%m-%d')).sum(:cargo_price)
 
   end
 

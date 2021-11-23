f093fd2e4bbc9239d6527a7d6bf4337129accf90
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-11-24 02:35:05 +0800

    fix report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,13 @@
 class ReportsController < ApplicationController
   def index
-    @reports = OrderDetail.where(finish_date: Time.now.strftime('%Y-%m-%d')).group('action_user_id').sum(:cargo_price)
-    @cargo_today = OrderDetail.where(finish_date: Time.now.strftime('%Y-%m-%d')).sum(:cargo_price)
-    @sold_total = Sold.where(sold_date: Time.now.strftime('%Y-%m-%d')).sum(:price)
+    @current_date = params[:date]
+    if @current_date.present?
+      puts 'date selected'
+    else
+      @current_date = Time.now.strftime('%Y-%m-%d')
+    end
+    @reports = OrderDetail.where(finish_date: @current_date).group('action_user_id').sum(:cargo_price)
+    @cargo_today = OrderDetail.where(finish_date: @current_date).sum(:cargo_price)
+    @sold_total = Sold.where(sold_date: @current_date).sum(:price)
   end
 end

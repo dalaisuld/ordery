5accaff35dcfe4b873814c5a0df931b493320862
dsukhochir <dsukhochir@gmail.com>
2022-03-23 10:42:24 +0800

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,32 +28,43 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     product.update({category_id: params[:category_id], user_id: current_user.id, name: params[:name], price: params[:price], total_amount: params[:quantity].to_i * params[:price].to_i, quantity: params[:quantity], ordered_count: params[:ordered_count], cargo: params[:cargo], unit: params[:unit], received_count: params[:received_count], rejected: params[:rejected]})
     LogsHelper.create("Product дээр бүтээгдэхүүн шинэчлэлээ #{product.id}", current_user.id)
-    quantity =  params[:quantity]
+    quantity =  params[:received_count]
     products = OrderDetail.where('status  IN (0,1) and product_id = :id', id: product.id)
     products.update_all(cargo_price: params[:cargo])
-    ordere_pr_count = products.select('sum(quantity) as total').first
-    puts "neeedz ===>#{ordere_pr_count.total.to_s.to_i} "
-    puts "quantity ===> #{quantity.to_s.to_i}"
-    if quantity.to_s.to_i >= ordere_pr_count.total.to_s.to_i
-      products.each do |product|
-        product.status = IS_WILLING
-        product.save
-        puts '--------------->>>>'
-      end
-    else
-      counter = 0
-      products.each do |product|
-        product.status = IS_WAITING
-        product.save
-      end
-      products.each do |product|
-        counter += product.quantity.to_s.to_i
-        product.status = IS_WILLING
-        product.save
-        puts "counter =====> #{counter}"
-        break if counter.to_i >= quantity.to_s.to_i
-      end
+
+    counter = 0
+    products.each do |product|
+      counter += product.quantity.to_s.to_i
+      product.status = IS_WILLING
+      product.save
+      puts "counter =====> #{counter}"
+      break if counter.to_i >= quantity.to_s.to_i
     end
+
+
+
+
+
+    # if quantity.to_s.to_i >= ordere_pr_count.total.to_s.to_i
+    #   products.each do |product|
+    #     product.status = IS_WILLING
+    #     product.save
+    #     puts '--------------->>>>'
+    #   end
+    # else
+    #   counter = 0
+    #   products.each do |product|
+    #     product.status = IS_WAITING
+    #     product.save
+    #   end
+    #   products.each do |product|
+    #     counter += product.quantity.to_s.to_i
+    #     product.status = IS_WILLING
+    #     product.save
+    #     puts "counter =====> #{counter}"
+    #     break if counter.to_i >= quantity.to_s.to_i
+    #   end
+    # end
     render json: { data: 'success'}
   end
 

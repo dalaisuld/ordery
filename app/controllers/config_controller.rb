78c2fc4fec329d62d761e421b3f4ce1b77a610b8
dsukhochir <dsukhochir@gmail.com>
2022-05-06 12:42:04 +0800

    fix

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -1,5 +1,16 @@
 class ConfigController < ApplicationController
+  before_action :authenticate_user!
   def index
     @page_config_active = true
+    @site_config = SiteConfig.first
+  end
+
+  def update
+    @site_config = SiteConfig.first
+    if params[:delivery_status] and params[:delivery_text]
+      @site_config.delivery_status = params[:delivery_status]
+      @site_config.delivery_text = params[:delivery_text]
+      @site_config.save
+    end
   end
 end

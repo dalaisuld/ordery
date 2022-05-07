eb351f509dd8dee66dad67920d58dbe58703e599
dsukhochir <dsukhochir@gmail.com>
2022-05-07 19:57:13 +0800

    fix

--- app/controllers/cargo_price_controller.rb
+++ app/controllers/cargo_price_controller.rb
@@ -2,7 +2,7 @@ class CargoPriceController < ApplicationController
   def index
     @page_cargo_price_active = true
     @products = Product.all
-    @products = @products.where.not(cargo: [nil])
+    @products = @products.where.not(cargo: [nil]).order('updated_at desc')
     if params[:q]
       @products = @products.where("name like ?", "%#{params[:q]}%")
     end

d45b11aa8a2bf5aae64ac90f46bf595fd05e91fa
dsukhochir <dsukhochir@gmail.com>
2022-05-16 20:52:44 +0800

    fix

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,13 +1,22 @@
 class Log < ApplicationRecord
   def self.search_by(search)
-    by_description(search['description'])
+    # by_description(search['description'])
+    by_description(search['description']).by_created_at(search['created_at'])
   end
 
   def self.by_description(description)
     if description.present?
-      where('description LIKE ?', "%#{description}%").order("created_at desc")
+      where('description LIKE ?', "%#{description}%").order("created_at asc")
     else
-      all
+      all.order("created_at desc")
+    end
+  end
+
+  def self.by_created_at(created_at)
+    if created_at.present?
+      where('created_at LIKE ?', "%#{created_at}%").order("created_at asc")
+    else
+      all.order("created_at asc")
     end
   end
 end

44ba55406ae0c5c4e316fbccc85716a52ba60519
sukh-ochir D <dsukhochir@gmail.com>
2022-10-22 13:10:52 +0800

    fix

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,7 +9,7 @@ class ReportsController < ApplicationController
     @reports = OrderDetail.where(finish_date: @current_date).group('action_user_id').sum(:cargo_price)
     @cargo_today = OrderDetail.where(finish_date: @current_date).sum(:cargo_price)
     @sold_total = Sold.where(sold_date: @current_date).sum('price * quantity')
-    sql = "select p.name, sum(od.quantity) from order_details as od inner join products as p on p.id = od.product_id where od.finish_date = '#{@current_date}' group by od.product_id;"
+    sql = "select p.id, p.name, sum(od.quantity) from order_details as od inner join products as p on p.id = od.product_id where od.finish_date = '#{@current_date}' group by od.product_id;"
     @sold_products = ActiveRecord::Base.connection.execute(sql)
 
 

bfcc06ab8285f98f331d734a3056d0e7c32d5ae9
sukh-ochir D <dsukhochir@gmail.com>
2022-11-05 08:46:03 +0800

    fix

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -144,11 +144,14 @@ class ClientsController < ApplicationController
     od.cargo_price,
     od.status').where('o.phone_number = :q', q: @client.phone_number.strip.to_s).order('status')
 
-    @total_cargo_price = @products.sum('p.price * p.quantity')
+    # @total_cargo_price = @products.sum('p.price * p.quantity')
+    @total_cargo_price = "0"
 
     # Нийт шилжүүлсэн мөнгөн дүн
-    @total_sent_amount = @orders.sum('amount')
-    @total_product_price = @products.sum('p.price * od.quantity')
+    @total_sent_amount = "0"
+    # @total_sent_amount = @orders.sum('amount')
+    # @total_product_price = @products.sum('p.price * od.quantity')
+    @total_product_price = '0'
     # @total_product_price = OrderDetail.where(order_id: params[:id]).sum('price * quantity')
 
   end
